abd091045cfa9353244fc709dcd5442c4614cc36
Mely Espino <90160548+mespino4@users.noreply.github.com>
2024-02-05 14:39:46 -0600

    update

---
+++ flask-server/app.py
@@ -0,0 +1,18 @@
+from models import db
+from routes import app
+from flask_cors import CORS
+
+# Configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///smolink.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize the database
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+# Enable CORS for all routes
+CORS(app, origins=["http://localhost:5173"])
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
---
+++ react-client/src/App.tsx
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import axios from 'axios';
+import clipboardCopy from 'clipboard-copy';
+import './App.css';
+
+function App() {
+  const [originalUrl, setOriginalUrl] = useState('');
+  const [shortenedUrl, setShortenedUrl] = useState('');
+  const [copied, setCopied] = useState(false);
+
+  const shortenUrl = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/shorten', { nm: originalUrl });
+      
+      if (response.data.short_url) {
+        //setShortenedUrl("https://smolink/" + response.data.short_url);
+        setShortenedUrl("http://localhost:5000/" + response.data.short_url);
+      } else {
+        console.error('Failed to shorten URL');
+      }
+    } catch (error) {
+      console.error('Error during URL shortening:', error);
+    }
+  };
+
+  const handleCopy = () => {
+    clipboardCopy(shortenedUrl);
+    setCopied(true);
+  };
+
+  return (
+    <div className="container">
+      <img
+        src="smolinkShadow.png"
+        className="logo"
+        alt="URL Shortener Logo"
+      />
+      <h1>Enter a URL</h1>
+
+      <div className="card">
+
+        <div className="input-container">
+          <input
+            type="url"
+            id="originalUrlInput"
+            placeholder="Enter URL"
+            value={originalUrl}
+            onChange={(e) => setOriginalUrl(e.target.value)}
+            required
+            className="long-input"
+          />
+        </div>
+        
+        <button onClick={shortenUrl} className="shorten-button">Shorten</button>
+
+        <div className="input-container">
+          <input
+            type="text"
+            id="shortenedUrlInput"
+            placeholder="Shortened URL"
+            value={shortenedUrl}
+            readOnly
+            className="long-input"
+            ref={(input) => input && input.setSelectionRange(0, input.value.length)}
+          />
+          <button onClick={handleCopy} className="copy-button" disabled={!shortenedUrl}>
+            {copied ? 'Copied!' : 'Copy'}
+          </button>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
