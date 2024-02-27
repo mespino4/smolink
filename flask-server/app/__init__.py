70a5d70402ca223e005fc055419da973cfd70df6
Mely Espino <90160548+mespino4@users.noreply.github.com>
2024-02-27 09:31:06 -0600

    production build

--- flask-server/app/__init__.py
+++ flask-server/app/__init__.py
@@ -1,3 +1,7 @@
 from flask import Flask
 
-app = Flask(__name__)
+#app = Flask(__name__)
+app = Flask(__name__, static_folder="../dist", static_url_path='/')
+
+@app.route('/')
+def index(): return app.send_static_file('index.html')
